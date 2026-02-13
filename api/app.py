43b2b27acdabd4e8d2f84b3bcbe790fe4888b023
Valentine DUMANGE <valentine.dumange@telecom-sudparis.eu>
2026-02-13 11:51:19 +0100

    API link to model local

--- api/app.py
+++ api/app.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from postgres import Postgres
+import mlflow
 
-app = FastAPI(title="Assistant Vocal Médical API", version="0.1.0")
+app = FastAPI(title="Assistant Vocal Médical API", version="0.2.0")
+
+# --- CONFIGURATION ---
+# Pour simplifier, on utilise un chemin local pour le modèle. En production, ce serait un URI MLflow.
+MODEL_PATH = "/mlartifacts/1/models/m-f2df07c11b2b480e8a70ca2a9d1324fb/artifacts"
 
 @app.get("/")
 def read_root():
-    return {"status": "alive", "version": "0.1.0"}
+    return {"status": "alive", "version": "0.2.0"}
 
 @app.get("/health")
 def health_check():
@@ -16,3 +21,9 @@ def get_appointments():
     db = Postgres("postgresql://user:password@db:5432/medical_db")
     slots = db.all("SELECT * FROM slots WHERE is_booked = FALSE;")
     return {"available_slots": slots}
+
+@app.post("/predict")
+def predict_intent(text: str):
+    model = mlflow.sklearn.load_model(MODEL_PATH)
+    intent = model.predict([text])[0]
+    return {"intent": intent}
